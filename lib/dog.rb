2100aa43037e20871fcadc2d358d13448e6e9ff5
KG ZZ <czheng5h88@gmail.com>
2019-09-19 23:59:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dogs_name)
+    @dogs_name = dogs_name
+  end 
+  def name
+    return @dogs_name
+  end 
+  def job=(dogs_job)
+    @dogs_job = dogs_job
+  def dogs_job 
+    return @dogs_job 
+  end 
+end 
+  
